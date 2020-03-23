5f66cd84e70775deb4c38d32c501b03cee4786ee
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-23 17:32:19 +0000

    add Avengers: Infinity War to movies

--- marvel.sql
+++ marvel.sql
@@ -59,6 +59,8 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 
 -- The cinema is showing ‘Batman Begins’, but Batman is DC, not Marvel! Delete the entry from the ‘movies’ table.
 DELETE FROM movies WHERE title = 'Batman Begins';
+-- The cinema has just heard that they will be holding an exclusive midnight showing of ‘Avengers: Infinity War’!! Create a new entry in the ‘movies’ table to reflect this.
+INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
 
 -- Return ALL the data in the ‘movies’ table.
 SELECT * FROM movies;
@@ -69,7 +71,6 @@ SELECT name FROM people WHERE name = 'Homer Simpson';
 
 
 
--- The cinema has just heard that they will be holding an exclusive midnight showing of ‘Avengers: Infinity War’!! Create a new entry in the ‘movies’ table to reflect this.
 -- The cinema would like to make the Iron Man movies a triple billing. Find out the show time of “Iron Man 2” and set the show time of “Iron Man 3” to start two hours later.
 -- Extension
 -- Research how to delete multiple entries from your table in a single command.
