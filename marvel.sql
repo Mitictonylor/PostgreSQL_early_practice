419acff225c34bb2d72a69d9b8eedaaf9bf3c219
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-24 10:13:15 +0000

    add another line of code for the iron man

--- marvel.sql
+++ marvel.sql
@@ -63,6 +63,7 @@ DELETE FROM movies WHERE title = 'Batman Begins';
 INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
 
 -- The cinema would like to make the Iron Man movies a triple billing. Find out the show time of “Iron Man 2” and set the show time of “Iron Man 3” to start two hours later.
+SELECT show_time FROM movies WHERE title  = 'Iron Man 2'
 UPDATE movies SET show_time = '20.45' WHERE title = 'Iron Man 3';
 
 -- Research how to delete multiple entries from your table in a single command.
